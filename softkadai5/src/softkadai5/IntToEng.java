038fe2e370dbab215ef30e1df93cef15b3c3651d
kojimanaoko <kojimanaoko@localhost>
2020-06-16 14:43:14 +0900

    エラー出てるけど３０分たったので送りますyo

--- softkadai5/src/softkadai5/IntToEng.java
+++ softkadai5/src/softkadai5/IntToEng.java
@@ -5,15 +5,25 @@ import java.util.Scanner;
 public class IntToEng {
     // メインメソッド
     public static void main(String[] args) {
+    	//とりあえず１００回できる
+    	for (int i = 1; i <= 100; i++) {	
         Scanner sc = new Scanner(System.in);
         System.out.print("数字を入力してください: ");
         int input = sc.nextInt();
         System.out.println(translateEng(input));
+    	}
     }
+    
 
     // 数値を英訳する変換するメソッド
-    static String translateEng(int n) {
+    public static String translateEng(int n) {
+    	
+    	
     	//一番目の人（じま）1~10までの範囲の出力
+    }
+    
+    public String show1(int n) {
+   
     	String no = "";
     	  switch (n) {
     	   case 0: break;
@@ -28,31 +38,50 @@ public class IntToEng {
     	   case 9: no = "nine"; break;
     	   case 10: no = "ten"; break;
     	  }
-    	  //return no;
-    //}
-    
-    //２桁の取扱い
-    
-    if(n>10) {
-    	
-    	 switch (n%10) {
-			
-    	 	case 0: break;
-			case 1: no = "eleven"; break;
-			case 2: no = "twelve"; break;
-			case 3: no = "thirteen"; break;
-			case 4: no = "fourteen"; break;
-			case 5: no = "fifteen"; break;
-			case 6: no = "sixteen"; break;
-			case 7: no = "seventeen"; break;
-			case 8: no = "eighteen"; break;
-			case 9: no = "nineteen"; break;
-			
-    	}
-    }
-    	return no;
+    	  return no;
     }
     	
+    public String showTeen(int n) {
+    	 // if(n>10) {
+    	String no = "";   
+    	  switch (n%10) { 
+    	   case 0: break;
+    	   case 1: no = "eleven"; break;
+    	   case 2: no = "twelve"; break;
+    	   case 3: no = "thirteen"; break;
+    	   case 4: no = "fourteen"; break;
+    	   case 5: no = "fifteen"; break;
+    	   case 6: no = "sixteen"; break;
+    	   case 7: no = "seventeen"; break;
+    	   case 8: no = "eighteen"; break;
+    	   case 9: no = "nineteen"; break;
+    	   
+    	     }
+    	    
+    	     return no;
+    }
+    public String show2(int n) {
+    	String no = "";
+		switch (n/10) {
+			case 0: break;
+			case 2: no = "twenty"; break;
+			case 3: no = "thirty"; break;
+			case 4: no = "forty"; break;
+			case 5: no = "fifty"; break;
+			case 6: no = "sixty"; break;
+			case 7: no = "seventy"; break;
+			case 8: no = "eighty"; break;
+			case 9: no = "ninety"; break;
+		}
+		return no;
+    }
 }
-	 
-  	
+	
+    	
+    
+    
+    	    
+    
+    	    
+    		
+    		
